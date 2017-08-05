4c3dc1efda06ae4746f2a3cc948955bac1c96ce7
Adam Oswalt <email@adamoswalt.com>
2017-08-05 16:08:35 -0500

    switch css to jss

--- src/App.js
+++ src/App.js
@@ -1,21 +1,42 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import logo from './logo.svg'
+import injectSheet from 'react-jss'
+
+const styles = {
+  app: {
+    'text-align': 'center',
+  },
+  logo: {
+    'animation': 'App-logo-spin infinite 20s linear',
+    'height': '80px',
+  },
+  header: {
+    'background-color': '#222',
+    'height': '150px',
+    'padding': '20px',
+    'color': 'white',
+  },
+  intro: {
+    'font-size': 'large',
+  },
+}
 
 class App extends Component {
   render() {
+    const { classes } = this.props
+
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
+      <div className={classes.app}>
+        <div className={classes.header}>
+          <img src={logo} className={classes.logo} alt='logo' />
           <h2>Welcome to React</h2>
         </div>
-        <p className="App-intro">
+        <p className={classes.intro}>
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
       </div>
-    );
+    )
   }
 }
 
-export default App;
+export default injectSheet(styles)(App)
