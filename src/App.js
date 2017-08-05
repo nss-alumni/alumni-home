aa2604b0259891a2d9a9d10f900353e222ffd1a1
Adam Oswalt <email@adamoswalt.com>
2017-08-05 16:44:30 -0500

    remove unused create-react-app boilerplate

--- src/App.js
+++ src/App.js
@@ -1,35 +1,15 @@
 import React from 'react'
 import injectSheet from 'react-jss'
-import logo from './logo.svg'
 
 const sheet = {
   app: {
     'text-align': 'center',
   },
-  logo: {
-    'animation': 'App-logo-spin infinite 20s linear',
-    'height': '80px',
-  },
-  header: {
-    'background-color': '#222',
-    'height': '150px',
-    'padding': '20px',
-    'color': 'white',
-  },
-  intro: {
-    'font-size': 'large',
-  },
 }
 
 const App = ({ classes }) => (
   <div className={classes.app}>
-    <div className={classes.header}>
-      <img src={logo} className={classes.logo} alt='logo' />
-      <h2>Welcome to React</h2>
-    </div>
-    <p className={classes.intro}>
-      To get started, edit <code>src/App.js</code> and save to reload.
-    </p>
+    <h1>NSS Alumni</h1>
   </div>
 )
 
