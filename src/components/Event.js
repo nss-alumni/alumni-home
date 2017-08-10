b4997c909f9b36506992e2e703ce20db56a6fc0a
Adam Oswalt <email@adamoswalt.com>
2017-08-09 20:50:52 -0500

    add more styling to events

--- src/components/Event.js
+++ src/components/Event.js
@@ -1,9 +1,19 @@
 import React from 'react'
 import injectSheet from 'react-jss'
+import { colors } from 'theme'
 
 const sheet = {
+  date: {
+    'color': colors.faintText,
+    'margin': 0,
+  },
   event: {
-    'margin': '0.5rem',
+    'margin-top': '0.5rem',
+    'margin-bottom': '0.5rem',
+  },
+  name: {
+    'text-size': '1.5rem',
+    'margin-bottom': '.25rem',
   },
 }
 
@@ -17,8 +27,8 @@ const Event = ({
   name,
 }) => (
   <div className={classes.event}>
-    <h2>{name}</h2>
-    <p>{date.format(dateFormat)}</p>
+    <h2 className={classes.name}>{name}</h2>
+    <p className={classes.date}>{date.format(dateFormat)}</p>
     <p>{description}</p>
     <a href={link}>More Info</a>
   </div>
