3d62e86aab240c62e912e9055013d2e05105d93d
Adam Oswalt <email@adamoswalt.com>
2017-08-09 20:44:46 -0500

    add more colors to theme need to think more about organizing/naming the colors

--- src/theme.js
+++ src/theme.js
@@ -1,9 +1,12 @@
 import * as palette from 'material-ui/colors'
 
 const colors = {
-  neutral: palette.grey[200],
+  neutral: palette.grey[300],
   white: palette.white,
   orange: palette.orange[600],
+  accent: palette.blueGrey[700],
+  text: palette.grey[900],
+  faintText: palette.grey[600],
 }
 
 export {
