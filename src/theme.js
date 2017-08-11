e740e9728eb4f96db0b63248e3ec395eab1e360e
Adam Oswalt <email@adamoswalt.com>
2017-08-10 20:13:53 -0500

    tweak theme colors and alphabetize

--- src/theme.js
+++ src/theme.js
@@ -1,12 +1,12 @@
 import * as palette from 'material-ui/colors'
 
 const colors = {
-  neutral: palette.grey[300],
-  white: palette.white,
-  orange: palette.orange[600],
   accent: palette.blueGrey[700],
-  text: palette.grey[900],
   faintText: palette.grey[600],
+  neutral: palette.grey[200],
+  orange: palette.orange[600],
+  text: palette.grey[900],
+  white: palette.blueGrey[50],
 }
 
 export {
