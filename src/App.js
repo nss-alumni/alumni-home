b1f5514fbff2c5a106a839dd98c60b730658ecab
Adam Oswalt <email@adamoswalt.com>
2017-08-26 18:19:28 -0500

    route to events page on bare route

--- src/App.js
+++ src/App.js
@@ -1,8 +1,17 @@
+import {
+  Route,
+  BrowserRouter as Router
+} from 'react-router-dom'
 import EventsPage from 'views/EventsPage'
 import React from 'react'
 
+// NOTE(adam): want to keep props in specific order for router
+/* eslint-disable react/jsx-sort-props */
 const App = () => (
-  <EventsPage />
+  <Router>
+    <Route exact path='/' component={EventsPage} />
+  </Router>
 )
+/* eslint-enable react/jsx-sort-props */
 
 export default App
