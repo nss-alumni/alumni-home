6a83176baf0fbb3b8669a3a900f8478b2df968cd
Adam Oswalt <email@adamoswalt.com>
2017-08-26 18:18:52 -0500

    move events page to views folder

--- src/App.js
+++ src/App.js
@@ -1,40 +1,8 @@
-import Donate from 'components/Donate'
-import EventList from 'components/EventList'
-import Header from 'components/Header'
-import PropTypes from 'prop-types'
+import EventsPage from 'views/EventsPage'
 import React from 'react'
-import Tile from 'components/Tile'
-import { colors } from 'theme'
-import events from 'eventList'
-import injectSheet from 'react-jss'
 
-const sheet = {
-  app: {
-    'color': colors.text,
-    'margin': '0 2.5rem',
-    'text-align': 'left',
-  },
-  tileContainer: {
-    'display': 'flex',
-  },
-}
-
-const App = ({ classes }) => (
-  <div className={classes.app}>
-    <Header />
-    <div className={classes.tileContainer}>
-      <Tile title='Upcoming Events'>
-        <EventList list={events} />
-      </Tile>
-      <Tile title='Donations'>
-        <Donate />
-      </Tile>
-    </div>
-  </div>
+const App = () => (
+  <EventsPage />
 )
 
-App.propTypes = {
-  classes: PropTypes.object.isRequired,
-}
-
-export default injectSheet(sheet)(App)
+export default App
---
+++ src/views/EventsPage.js
@@ -0,0 +1,40 @@
+import Donate from 'components/Donate'
+import EventList from 'components/EventList'
+import Header from 'components/Header'
+import PropTypes from 'prop-types'
+import React from 'react'
+import Tile from 'components/Tile'
+import { colors } from 'theme'
+import events from 'eventList'
+import injectSheet from 'react-jss'
+
+const sheet = {
+  app: {
+    'color': colors.text,
+    'margin': '0 2.5rem',
+    'text-align': 'left',
+  },
+  tileContainer: {
+    'display': 'flex',
+  },
+}
+
+const EventPage = ({ classes }) => (
+  <div className={classes.app}>
+    <Header />
+    <div className={classes.tileContainer}>
+      <Tile title='Upcoming Events'>
+        <EventList list={events} />
+      </Tile>
+      <Tile title='Donations'>
+        <Donate />
+      </Tile>
+    </div>
+  </div>
+)
+
+EventPage.propTypes = {
+  classes: PropTypes.object.isRequired,
+}
+
+export default injectSheet(sheet)(EventPage)
