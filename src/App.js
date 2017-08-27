409f331083e13343c3fb5a8def985106da892c05
Adam Oswalt <email@adamoswalt.com>
2017-08-26 19:23:06 -0500

    set router basename

--- src/App.js
+++ src/App.js
@@ -8,8 +8,8 @@ import React from 'react'
 // NOTE(adam): want to keep props in specific order for router
 /* eslint-disable react/jsx-sort-props */
 const App = () => (
-  <Router>
-    <Route exact path='/' component={EventsPage} />
+  <Router basename='/alumni-home'>
+    <Route path='/' component={EventsPage} />
   </Router>
 )
 /* eslint-enable react/jsx-sort-props */
