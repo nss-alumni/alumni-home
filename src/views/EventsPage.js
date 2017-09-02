fc9ba7c6f80e4c9422d1f0eb5891d83cf3cb2ff4
Adam Oswalt <email@adamoswalt.com>
2017-09-02 15:55:33 -0500

    move events into EventPage component state

--- src/views/EventsPage.js
+++ src/views/EventsPage.js
@@ -2,10 +2,9 @@ import Donate from 'components/Donate'
 import EventList from 'components/EventList'
 import Header from 'components/Header'
 import PropTypes from 'prop-types'
-import React from 'react'
+import React, { PureComponent } from 'react'
 import Tile from 'components/Tile'
 import { colors } from 'theme'
-import events from 'eventList'
 import injectSheet from 'react-jss'
 
 const sheet = {
@@ -19,19 +18,64 @@ const sheet = {
   },
 }
 
-const EventPage = ({ classes }) => (
-  <div className={classes.app}>
-    <Header />
-    <div className={classes.tileContainer}>
-      <Tile title='Upcoming Events'>
-        <EventList list={events} />
-      </Tile>
-      <Tile title='Donations'>
-        <Donate />
-      </Tile>
-    </div>
-  </div>
-)
+class EventPage extends PureComponent {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      events: [
+        {
+          startDate: '2017-8-5',
+          description: 'The description of the event',
+          link: 'http://google.com',
+          name: 'Event name',
+        },
+        {
+          startDate: '2017-8-6',
+          description: 'The description of the event',
+          link: 'http://google.com',
+          name: 'Event name 2',
+        },
+        {
+          startDate: '2017-8-7',
+          description: 'The description of the event',
+          link: 'http://google.com',
+          name: 'Event name 3',
+        },
+        {
+          startDate: '2017-8-8',
+          description: 'The description of the event',
+          link: 'http://google.com',
+          name: 'Event name 4',
+        },
+        {
+          startDate: '2017-8-9',
+          description: 'The description of the event',
+          link: 'http://google.com',
+          name: 'Event name 5',
+        },
+      ],
+    }
+  }
+
+  render = () => {
+    const { classes } = this.props
+
+    return (
+      <div className={classes.app}>
+        <Header />
+        <div className={classes.tileContainer}>
+          <Tile title='Upcoming Events'>
+            <EventList list={this.state.events} />
+          </Tile>
+          <Tile title='Donations'>
+            <Donate />
+          </Tile>
+        </div>
+      </div>
+    )
+  }
+}
 
 EventPage.propTypes = {
   classes: PropTypes.object.isRequired,
