808f1548c09f14344955291e3cc0449227677891
Adam Oswalt <email@adamoswalt.com>
2018-02-14 17:46:37 -0600

    Autofix import order

--- src/components/Tile.js
+++ src/components/Tile.js
@@ -1,7 +1,7 @@
+import { colors } from 'theme'
 import Paper from 'material-ui/Paper'
 import PropTypes from 'prop-types'
 import React from 'react'
-import { colors } from 'theme'
 import injectSheet from 'react-jss'
 
 const sheet = {
