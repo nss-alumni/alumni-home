7670c611b5ed5485bfe0e35c851ef88ec82f23f7
Adam Oswalt <email@adamoswalt.com>
2018-03-15 08:38:40 -0500

    Combine prop types for common import
    Having both sets of prop types in a single export removes the need to
    differentiate between which import to use, and the prop type names do
    not conflict with one another.

--- src/components/Donate.js
+++ src/components/Donate.js
@@ -1,4 +1,4 @@
-import PropTypes from 'prop-types'
+import PropTypes from 'utils/propTypes'
 import React from 'react'
 import Typography from 'material-ui/Typography'
 import injectSheet from 'react-jss'
--- src/components/Tile.js
+++ src/components/Tile.js
@@ -1,5 +1,5 @@
 import Paper from 'material-ui/Paper'
-import PropTypes from 'prop-types'
+import PropTypes from 'utils/propTypes'
 import React from 'react'
 import Typography from 'material-ui/Typography'
 import injectSheet from 'react-jss'
