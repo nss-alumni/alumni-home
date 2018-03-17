711325349db15452650a27ffb3409fb8b72f475e
Adam Oswalt <email@adamoswalt.com>
2018-03-17 16:17:43 -0500

    Move the root reducer to root.js
    Moving the root reducer to a separate file keeps the store setup
    isolated from the reducer setup.

--- src/store.js
+++ src/store.js
@@ -1,17 +1,12 @@
 import { applyMiddleware, compose, createStore } from 'redux'
-import { combineReducers } from 'redux-immutable'
+import { rootReducer } from './root'
 import errorLogger from 'middleware/errorLogger'
-import events from 'data/events'
 import fsaLinter from 'middleware/fsaLinter'
-
-const reducer = combineReducers({
-  events,
-})
 
 // NOTE(adam): if the redux extension exists, attach it to the store
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
 
 export default createStore(
-  reducer,
+  rootReducer,
   composeEnhancers(applyMiddleware(fsaLinter, errorLogger)),
 )
