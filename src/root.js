0ad9cbd67a99f30451efede6e7dbcd03eaaaad27
Adam Oswalt <email@adamoswalt.com>
2018-03-17 16:45:16 -0500

    Wire up redux-observable
    Epics are ultimately combined into a root epic as with reducers.
    
    The epic middleware takes the rootEpic and is applied with the other
    middleware.

--- src/root.js
+++ src/root.js
@@ -1,5 +1,8 @@
+import { combineEpics } from 'redux-observable'
 import { combineReducers } from 'redux-immutable'
 import events from 'data/events'
+
+export const rootEpic = combineEpics()
 
 export const rootReducer = combineReducers({
   events,
--- src/store.js
+++ src/store.js
@@ -1,12 +1,15 @@
 import { applyMiddleware, compose, createStore } from 'redux'
-import { rootReducer } from './root'
+import { createEpicMiddleware } from 'redux-observable'
+import { rootEpic, rootReducer } from './root'
 import errorLogger from 'middleware/errorLogger'
 import fsaLinter from 'middleware/fsaLinter'
 
 // NOTE(adam): if the redux extension exists, attach it to the store
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
 
+const epicMiddleware = createEpicMiddleware(rootEpic)
+
 export default createStore(
   rootReducer,
-  composeEnhancers(applyMiddleware(fsaLinter, errorLogger)),
+  composeEnhancers(applyMiddleware(fsaLinter, errorLogger, epicMiddleware)),
 )
