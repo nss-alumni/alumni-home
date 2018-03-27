d9792cc6f0e61836f8ed8170f3058f55fd6d8a5a
Adam Oswalt <email@adamoswalt.com>
2018-03-26 22:04:15 -0500

    Add alumni data module

--- src/root.js
+++ src/root.js
@@ -1,14 +1,21 @@
 import { combineEpics } from 'redux-observable'
 import { combineReducers } from 'redux-immutable'
+import alumni, { fetchAlumniEpic } from 'data/alumni'
 import errorSnackbar from 'epics/errorSnackbar'
 import events, { fetchEventsEpic } from 'data/events'
 import isFetchingEvents from 'data/isFetchingEvents'
 import navigation from 'epics/navigation'
 import snackbarErrorMessage from 'data/snackbarErrorMessage'
 
-export const rootEpic = combineEpics(fetchEventsEpic, navigation, errorSnackbar)
+export const rootEpic = combineEpics(
+  fetchAlumniEpic,
+  fetchEventsEpic,
+  navigation,
+  errorSnackbar,
+)
 
 export const rootReducer = combineReducers({
+  alumni,
   events,
   isFetchingEvents,
   snackbarErrorMessage,
