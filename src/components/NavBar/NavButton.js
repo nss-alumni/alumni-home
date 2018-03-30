953b8645f5fb4b45cdc94e164f9a50330808523f
Adam Oswalt <email@adamoswalt.com>
2018-03-30 10:54:37 -0500

    Allow href instead of location/to for NavButton
    For external links, only the href needs to be passed into the NavButton,
    internally converting them into normal a tags. This should be an
    enforceable condition on the propTypes or at least a warning.

--- src/components/NavBar/NavButton.js
+++ src/components/NavBar/NavButton.js
@@ -10,26 +10,32 @@ const styles = _theme => ({
   },
 })
 
-const NavButton = ({ active, location, to, classes, ...props }) => (
+const NavButton = ({ active, href, location, to, classes, ...props }) => (
   <Button
     classes={{ label: classes.buttonText }}
     color={location && location.pathname.includes(to) ? 'primary' : 'inherit'}
-    component={Link}
+    component={href ? undefined : Link}
+    href={href}
     location={location}
     to={to}
     {...props}
   />
 )
 
+// NOTE(adam): if href, we don't need location and to. This should be enforceable
 NavButton.propTypes = {
   active: PropTypes.any,
   classes: PropTypes.object.isRequired,
-  location: PropTypes.object.isRequired,
-  to: PropTypes.oneOf([PropTypes.string, PropTypes.object]).isRequired,
+  href: PropTypes.string,
+  location: PropTypes.object,
+  to: PropTypes.oneOf([PropTypes.string, PropTypes.object]),
 }
 
 NavButton.defaultProps = {
   active: false,
+  href: undefined,
+  location: undefined,
+  to: undefined,
 }
 
 export default injectSheet(styles)(NavButton)
