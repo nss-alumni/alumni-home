197094f97ed2c4327294cfa2347d56699f75b341
Adam Oswalt <email@adamoswalt.com>
2018-05-10 21:36:21 -0500

    Set home page to horizontal flex

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -3,13 +3,17 @@ import { homePageNavigated } from 'data/navigation'
 import InvolvementTile from './InvolvementTile'
 import NewsletterTile from './NewsletterTile'
 import PropTypes from 'utils/propTypes'
-import React, { Component, Fragment } from 'react'
+import React, { Component } from 'react'
 import injectSheet from 'react-jss'
 
 /* eslint-disable no-magic-numbers */
 const styles = ({ spacing }) => ({
+  container: {
+    display: 'flex',
+  },
   tile: {
     margin: spacing.unit * 2,
+    flex: 1,
   },
 })
 /* eslint-enable no-magic-numbers */
@@ -23,10 +27,10 @@ class AboutPage extends Component {
     const { classes } = this.props
 
     return (
-      <Fragment>
+      <div className={classes.container}>
         <InvolvementTile className={classes.tile} />
         <NewsletterTile className={classes.tile} />
-      </Fragment>
+      </div>
     )
   }
 }
