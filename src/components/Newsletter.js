c81cb3b86fcf9aa82a8f6088e90c99f4462445d6
Adam Oswalt <email@adamoswalt.com>
2018-05-23 23:29:53 -0500

    Update format of newsletter date

--- src/components/Newsletter.js
+++ src/components/Newsletter.js
@@ -2,11 +2,16 @@ import { Newsletter as NewsletterRecord } from 'data/newsletters'
 import { Typography } from 'material-ui'
 import PropTypes from 'utils/propTypes'
 import React from 'react'
+import moment from 'moment'
+
+const dateFormat = 'MMMM Do YYYY'
 
 const Newsletter = ({ className, newsletter }) => (
   <div className={className}>
     <Typography variant="title">{newsletter.subject}</Typography>
-    <Typography variant="subheading">{newsletter.sentDate}</Typography>
+    <Typography variant="caption">
+      {moment(newsletter.sentDate).format(dateFormat)}
+    </Typography>
     <Typography>{newsletter.body}</Typography>
   </div>
 )
