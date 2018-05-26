c17ed1d5d83f426be749b159735a6bf04fc81ddf
Adam Oswalt <email@adamoswalt.com>
2018-05-26 15:12:04 -0500

    Drop description from event list
    Including event descriptions takes up too much room on the home page.

--- src/components/Event.js
+++ src/components/Event.js
@@ -23,13 +23,6 @@ const Event = ({ classes, event }) => (
     <Typography variant="caption">
       {moment(event.startTime).format(dateFormat)}
     </Typography>
-    <Typography
-      dangerouslySetInnerHTML={{
-        __html:
-          event.description &&
-          event.description.replace(/(<? *script)/gi, 'illegalscript'),
-      }}
-    />
     <Button color="primary" href={event.link}>
       More Info
     </Button>
