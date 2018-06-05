ff4db2f98f5fec9a6c553d814011bb143c21c122
Adam Oswalt <email@adamoswalt.com>
2018-06-04 21:14:30 -0500

    Integrate react markdown with material ui styling

--- src/components/Newsletter.js
+++ src/components/Newsletter.js
@@ -3,24 +3,44 @@ import { Typography } from 'material-ui'
 import PropTypes from 'utils/propTypes'
 import React from 'react'
 import ReactMarkdown from 'react-markdown'
+import injectSheet from 'react-jss'
 import moment from 'moment'
+
+const styles = _theme => ({
+  heading: {
+    fontSize: '1.07rem',
+    marginTop: '.5rem',
+    marginBottom: '.3rem',
+  },
+})
 
 const dateFormat = 'MMMM Do YYYY'
 
-const Newsletter = ({ className, newsletter }) => (
+/* eslint-disable react/display-name */
+const nodeRenderers = classes => ({
+  paragraph: p => <Typography {...p} />,
+  heading: h => (
+    <Typography className={classes.heading} variant="title" {...h} />
+  ),
+})
+/* eslint-enable react/display-name */
+
+const Newsletter = ({ className, classes, newsletter }) => (
   <div className={className}>
     <Typography variant="title">{newsletter.subject}</Typography>
     <Typography variant="caption">
       {moment(newsletter.sentDate).format(dateFormat)}
     </Typography>
-    <Typography>
-      <ReactMarkdown source={newsletter.body} />
-    </Typography>
+    <ReactMarkdown
+      renderers={nodeRenderers(classes)}
+      source={newsletter.body}
+    />
   </div>
 )
 
 Newsletter.propTypes = {
   className: PropTypes.string,
+  classes: PropTypes.object.isRequired,
   newsletter: PropTypes.instanceOf(NewsletterRecord).isRequired,
 }
 
@@ -28,4 +48,4 @@ Newsletter.defaultProps = {
   className: undefined,
 }
 
-export default Newsletter
+export default injectSheet(styles)(Newsletter)
