763827c819ee3b6e0373e5535b76c127ddaba52a
Adam Oswalt <email@adamoswalt.com>
2018-11-25 16:35:49 -0600

    Change navigation api calls to be until success
    By using `first`, any failed api calls would never be able to complete.

--- src/epics/navigation.js
+++ src/epics/navigation.js
@@ -1,24 +1,29 @@
 import { ABOUT_PAGE_NAVIGATED, HOME_PAGE_NAVIGATED } from 'data/navigation'
+import { Observable } from 'rxjs/Rx'
 import { combineEpics } from 'redux-observable'
 import { fetchAlumni } from 'data/alumni'
 import { fetchEvents } from 'data/events'
 import { fetchInvolvements } from 'data/involvements'
 import { fetchNewsletters } from 'data/newsletters'
 
-export const aboutNavigation = action$ =>
-  action$
-    .ofType(ABOUT_PAGE_NAVIGATED)
-    .first()
-    .mapTo(fetchAlumni.request())
+// TODO(adam): this could be generalized with rxjs
+const requestUntilSuccess = action$ => request =>
+  action$.mapTo(request.request()).takeUntil(action$.ofType(request.SUCCEEDED))
 
-export const homeNavigation = action$ =>
-  action$
-    .ofType(HOME_PAGE_NAVIGATED)
-    .first()
-    .mergeMap(() => [
-      fetchEvents.request(),
-      fetchInvolvements.request(),
-      fetchNewsletters.request(),
-    ])
+export const aboutNavigation = action$ => {
+  const navigation$ = action$.ofType(ABOUT_PAGE_NAVIGATED)
+
+  return requestUntilSuccess(navigation$)(fetchAlumni)
+}
+
+export const homeNavigation = action$ => {
+  const navigation$ = action$.ofType(HOME_PAGE_NAVIGATED)
+
+  const apiCalls = [fetchEvents, fetchInvolvements, fetchNewsletters].map(
+    requestUntilSuccess(navigation$),
+  )
+
+  return Observable.merge(...apiCalls)
+}
 
 export default combineEpics(aboutNavigation, homeNavigation)
