c20d3aa925b8cd12c7a6d8c85742bd084eb8aecf
Adam Oswalt <email@adamoswalt.com>
2018-11-24 19:57:25 -0600

    Use bare payload for snackbarErrorMessage

--- src/data/snackbarErrorMessage.js
+++ src/data/snackbarErrorMessage.js
@@ -9,12 +9,12 @@ export const SET_MESSAGE = modType('SET_MESSAGE')
 export const CLEAR_MESSAGE = modType('CLEAR_MESSAGE')
 
 // ACTION CREATORS
-export const setMessage = creator(SET_MESSAGE, ['message'])
+export const setMessage = creator(SET_MESSAGE)
 export const clearMessage = creator(CLEAR_MESSAGE)
 
 // REDUCER
 export default createReducer(null, {
-  [SET_MESSAGE]: (_state, { payload: { message } }) => message,
+  [SET_MESSAGE]: (_state, { payload }) => payload,
   [CLEAR_MESSAGE]: _state => null,
 })
 
