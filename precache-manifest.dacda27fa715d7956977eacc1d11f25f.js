261a80ecf45e7132c53af742dafdf67ff8413c7f
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-01-22 00:40:01 +0000

    Deploy nss-alumni/alumni-home to github.com/nss-alumni/alumni-home.git:gh-pages

---
+++ precache-manifest.dacda27fa715d7956977eacc1d11f25f.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "0b6dca2125785e71490b",
+    "url": "/alumni-home/static/js/main.0b6dca21.chunk.js"
+  },
+  {
+    "revision": "cad3e95646fb998986ec",
+    "url": "/alumni-home/static/js/1.cad3e956.chunk.js"
+  },
+  {
+    "revision": "2fdc5dd4689520e85b64",
+    "url": "/alumni-home/static/js/runtime~main.2fdc5dd4.js"
+  },
+  {
+    "revision": "9cffb7e267d22bb6f77d65c0d05c4c89",
+    "url": "/alumni-home/index.html"
+  }
+];
